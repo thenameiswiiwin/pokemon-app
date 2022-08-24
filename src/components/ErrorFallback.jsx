fdb1df19ed7db59590be4213390fb5384064a1d2
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-08-24 12:46:49 -0500

    added ErrorBoundary and FallbackBoundary to App (#26)

--- src/components/ErrorFallback.jsx
+++ src/components/ErrorFallback.jsx
@@ -1,8 +1,14 @@
-function ErrorFallback({ error }) {
+function ErrorFallback({ error, resetErrorBoundary }) {
   return (
-    <div role="alert">
+    <div className="font-medium" role="alert">
       <h3 className="font-bold">There was an error:</h3>
-      <p className="mt-2 font-medium whiteSpace-normal">{error.message}</p>
+      <p className="mt-3 whiteSpace-normal">{error.message}</p>
+      <button
+        className="mt-3 py-1.5 px-2.5 rounded-md border border-solid bg-red-600 text-white hover:bg-red-700"
+        onClick={resetErrorBoundary}
+      >
+        Try again
+      </button>
     </div>
   )
 }
